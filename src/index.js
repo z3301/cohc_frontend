a665947372f566251bce3f486be79df66e7db558
z3301 <98485231+z3301@users.noreply.github.com>
2022-11-20 16:04:39 -0500

    fixed dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3009";
+axios.defaults.baseURL = "https://cohc-server.herokuapp.com";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
